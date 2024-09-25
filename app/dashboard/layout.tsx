4c4715183f0df5a1f80b3e67384d1d57050e3760
Muhammad Jafar Ash Siddiq <mjafarashsiddiq@gmail.com>
2024-09-25 20:45:58 +0700

    setup auth

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -2,9 +2,5 @@
 import { redirect } from "next/navigation";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  const token = localStorage.getItem("token");
-  if (!token) {
-    redirect("/login");
-  }
   return <div>{children}</div>;
 }
